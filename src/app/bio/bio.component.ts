69d3d92dfcbf32a56aa40cca7e6a6bfc9b8a22ad
Samin Azhan <34473594+Samin005@users.noreply.github.com>
2021-05-23 18:50:22 +0600

    Updated bio and added typed.js

--- src/app/bio/bio.component.ts
+++ src/app/bio/bio.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-bio',
@@ -8,15 +9,25 @@ import {Component, OnInit} from '@angular/core';
 export class BioComponent implements OnInit {
 
   bio = 'Hi there, my name is Samin Azhan.' +
-    ' I\'m currently working as a lecturer in BRAC University, Bangladesh.' +
-    ' I usually teach courses related Artificial Intelligence and Computer Science in my university.' +
-    ' Previously I worked as a Full-stack Software Engineer in one of the largest private groups in Bangladesh.' +
-    ' AI and Software Engineering are my primary interests, and I look forward to sharing my ideas.';
-
+    ' I\'m a lecturer in the Computer Science and Engineering department of BRAC University, Bangladesh.' +
+    ' I usually teach courses related Artificial Intelligence, Software Engineering and Data Science in my university.' +
+    ' Previously I worked as a Full-stack Software Engineer in one of the largest private companies in the country.' +
+    ' AI and Software Engineering are my primary interests, and I look forward in utilizing my skills, knowledge and experience in the field of research and practical applications.';
+  typedOptions = {
+    strings: [this.bio.split('.').join('.^1000')],
+    typeSpeed: 20,
+    backSpeed: 5,
+    backDelay: 300000,
+    showCursor: true,
+    cursorChar: '_',
+    loop: true
+  };
   constructor() {
   }
 
   ngOnInit(): void {
+    const typedBio = new Typed('#typed-bio', this.typedOptions);
+    typedBio.start();
   }
 
 }
