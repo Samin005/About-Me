09d9a2274c5a145f74f85631a062cd551336fda9
Samin Azhan <34473594+Samin005@users.noreply.github.com>
2021-06-01 00:13:52 +0600

    Added ML skills.

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -10,25 +10,30 @@ export class SkillsComponent implements OnInit {
   skillsString = '● Python\n' +
     '● Django, DRF\n' +
     '● ORM, OOP\n' +
-    '● Machine Learning\n' +
+    '● PyQt5\n' +
+    '● NumPy, Pandas\n' +
     '● Angular 2+\n' +
     '● Typescript, RxJS\n' +
     '● Java, JPA\n' +
     '● Spring Boot\n' +
+    '● scikit-learn, joblib\n' +
     '● REST\n' +
     '● HTML, CSS\n' +
     '● JS, jQuery\n' +
     '● JWT\n' +
+    '● seaborn\n' +
     '● Oracle, MySQL\n' +
     '● PostgreSQL\n' +
     '● Firebase\n' +
-    '● Git';
+    '● Git\n' +
+    '● Matplotlib';
   skills = this.skillsString.split('\n');
 
   constructor() {
   }
 
   ngOnInit(): void {
+    console.log(this.skills.length);
   }
 
 }
