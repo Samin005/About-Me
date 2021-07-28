5a0b791f0ba452b6ee8fabe47ad33e75720e6408
Samin Azhan <34473594+Samin005@users.noreply.github.com>
2021-07-29 00:09:36 +0600

    Added Kaggle Intermediate Machine Learning Certificate.

--- src/app/certifications/certifications.component.ts
+++ src/app/certifications/certifications.component.ts
@@ -9,6 +9,12 @@ export class CertificationsComponent implements OnInit {
 
   certifications = [
     {
+      title: 'Kaggle certified Intermediate Machine Learning',
+      img: 'assets/img/certifications/Samin%20Azhan%20-%20Intermediate%20Machine%20Learning.png',
+      showViewButton: true,
+      url: 'https://www.kaggle.com/learn/certification/saminazhan/intermediate-machine-learning'
+    },
+    {
       title: 'Kaggle certified Intro to Machine Learning',
       img: 'assets/img/certifications/Samin%20Azhan%20-%20Intro%20to%20Machine%20Learning.png',
       showViewButton: true,
