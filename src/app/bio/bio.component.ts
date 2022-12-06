9863f57dbb6d1eb799a42d985759f22ad8176cea
Samin Azhan <34473594+Samin005@users.noreply.github.com>
2022-12-06 17:18:53 -0500

    Update bio.component.ts

--- src/app/bio/bio.component.ts
+++ src/app/bio/bio.component.ts
@@ -12,11 +12,11 @@ export class BioComponent implements OnInit {
 
   bio = 'Hi there, my name is Samin Azhan.' +
     ' I\'m a Full-stack Software Engineer who is currently pursuing a Master\'s degree at Carleton University, Canada.' +
-    ' I love working with frameworks such as front-end: Angular (Typescript), back-end: Sprint Boot (Java), Django (Python), databases: Oracle, PostgreSQL, Firebase, MongoDB and REST APIs.' +
-    ' I am also experienced in using Test Automation tools such as JUnit, Cucumber, REST-assured, Selenium, WebdriverIO and have developed projects using Test-driven development (TDD) and behavior-driven development (BDD) in an Agile environment.' +
+    ' I love working with frameworks such as front-end: Angular (TypeScript), VanillaJS (JavaScript), Flutter (Dart), back-end: Sprint Boot (Java), Django (Python), databases: Oracle, PostgreSQL, Firebase, MongoDB and REST APIs.' +
+    ' I am also experienced in using Test Automation libraries such as JUnit, Cucumber, REST-assured, Selenium, WebdriverIO and have developed projects using Test-driven development (TDD) and Behavior-driven development (BDD) in an Agile environment.' +
     ' I am an AI enthusiast and worked with different machine learning libraries such as: scikit-learn, NumPy, Pandas, joblib, Matplotlib, seaborn and so on.' +
     ' I also worked a full-time University Lecturer and taught courses related with Artificial Intelligence, Software Engineering and Data Science.' +
-    ' AI and Software Engineering are my primary interests, and I look forward in utilizing my skills, knowledge and experience in the field of research and practical applications.';
+    ' Software Engineering and Machine Learning are my primary interests, and I look forward in utilizing my skills, knowledge and experience in the field of research and practical applications.';
   typedOptions = {
     strings: [this.bio.split('.').join('.^1000')],
     typeSpeed: 20,
