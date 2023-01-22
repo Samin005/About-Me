d8a60560cb19f9322f3b57bbd72756351442950a
Samin Azhan <34473594+Samin005@users.noreply.github.com>
2023-01-21 20:55:51 -0500

    Updated bio

--- src/app/bio/bio.component.ts
+++ src/app/bio/bio.component.ts
@@ -12,10 +12,10 @@ export class BioComponent implements OnInit {
 
   bio = 'Hi there, my name is Samin Azhan.' +
     ' I\'m a Full-stack Software Engineer who is currently pursuing a Master\'s degree at Carleton University, Canada.' +
-    ' I love working with frameworks such as front-end: Angular (TypeScript), VanillaJS (JavaScript), Flutter (Dart), back-end: Sprint Boot (Java), Django (Python), databases: Oracle, PostgreSQL, Firebase, MongoDB and REST APIs.' +
-    ' I am also experienced in using Test Automation libraries such as JUnit, Cucumber, REST-assured, Selenium, WebdriverIO and have developed projects using Test-driven development (TDD) and Behavior-driven development (BDD) in an Agile environment.' +
-    ' I am an AI enthusiast and worked with different machine learning libraries such as: scikit-learn, NumPy, Pandas, joblib, Matplotlib, seaborn and so on.' +
-    ' I also worked a full-time University Lecturer and taught courses related with Artificial Intelligence, Software Engineering and Data Science.' +
+    ' I am experienced in working with Software Development frameworks such as Angular (TypeScript), VanillaJS (JavaScript) and Flutter (Dart) for front-end; Sprint Boot (Java), Django (Python) and .NET (C#) for back-end; databases such as MySQL, Oracle, PostgreSQL, Firebase and MongoDB; as well as REST APIs.' +
+    ' I also have experience in using Test Automation libraries such as JUnit, Cucumber, REST-assured, Selenium, WebdriverIO and have developed projects using Test-Driven Development (TDD) and Behavior-Driven Development (BDD) in an Agile environment.' +
+    ' I am an AI enthusiast and worked with different machine learning libraries such as scikit-learn, NumPy, Pandas, joblib, Matplotlib, seaborn and TensorFlow.' +
+    ' I also worked as a full-time University Lecturer and taught courses related with Artificial Intelligence, Software Engineering and Data Science.' +
     ' Software Engineering and Machine Learning are my primary interests, and I look forward in utilizing my skills, knowledge and experience in the field of research and practical applications.';
   typedOptions = {
     strings: [this.bio.split('.').join('.^1000')],
